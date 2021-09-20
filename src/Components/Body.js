d41d2c7f6868557d8027b17d770bdc85180bdb17
Kevin Su <kevinsu@outlook.co.nz>
2021-09-20 12:02:07 +1200

    fully functional clock just looks ugly

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -3,21 +3,36 @@ import Clock from './Clock';
 
 function Body() {
 
-    const [time, setTime] = useState();
+    const [time, setTime] = useState("10:10:10 AM");
+    const [second, setSecond] = useState();
+    const [minute, setMinute] = useState();
+    const [hour, setHour] = useState();
 
     useEffect(() => {
+        console.log(new Date().toLocaleTimeString().split(" ")[0].split(":")[0])
         setInterval(() => getTime(), 1000);
     });
 
     const getTime = () => {
         var currTime = new Date().toLocaleTimeString();
+        var currHour = currTime.split(" ")[0].split(":")[0];
+        var currMinute = currTime.split(" ")[0].split(":")[1];
+        var currSecond = currTime.split(" ")[0].split(":")[2];
         setTime(currTime);
+        setHour(currHour);
+        setMinute(currMinute);
+        setSecond(currSecond);
     }
 
     return (
         <div className="container">
             <p>Current time is: {time}</p>
-            <Clock time = {time} />
+            <p>Curr hour is: {hour} {minute} {second}</p>
+            <Clock
+                hour={hour}
+                minute={minute}
+                second={second}
+            />
         </div>
     );
 }
--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-function Clock({time}) {
+function Clock({hour, minute, second}) {  
     return (
         <>
             <div className="clock">
-                <div className="hourHand" style = {{transform:`rotate`}}>
+                <div className="hourHand" style = {{transform:`rotate(${hour * 30 - 180}deg)`}}>
                     
                 </div>
+                <div className="minuteHand" style = {{transform:`rotate(${minute * 6 - 180}deg)`}}>
+                    
+                </div>
+                <div className="secondHand" style = {{transform:`rotate(${second * 6 - 180}deg)`}}>
+                    
+                </div>
+                <div className="center"></div>
             </div>
-            <p>{time}</p>
         </>
     );
 }
