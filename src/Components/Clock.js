cc0bb584f8d090b5a9a193d1ca98f0a72229c0c0
Kevin Su <kevinsu@outlook.co.nz>
2021-09-20 12:44:00 +1200

    small fix

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -7,7 +7,7 @@ function Clock({hour, minute, second}) {
     return (
         <>
             <div className="clock">
-                <div className="hourHand" style = {{transform:`rotate(${hour * 30 - 180}deg)`}}>
+                <div className="hourHand" style = {{transform:`rotate(${hour * 30 - 180 + (minute/60)*30}deg)`}}>
                     
                 </div>
                 <div className="minuteHand" style = {{transform:`rotate(${minute * 6 - 180}deg)`}}>
